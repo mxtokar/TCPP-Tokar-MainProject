80ca3b595d6cab09b548f4b25036a3a281517921
Maksym Tokar <mxtokar@gmail.com>
2021-04-19 12:40:46 +0300

    Добавьте файлы проекта.

---
+++ Project.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    a = 1;
+    b = 3;
+    c = a + b;
+
+    cout << "Hello World!\n";
+
+    cout << c;
+
+    return 0;
+}
